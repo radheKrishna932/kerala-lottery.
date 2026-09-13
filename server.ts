6ba020ad9e8661477f924f6f3e40844056d85ed2
radheKrishna932 <rkrishna0947@gmail.com>
2026-09-13 15:00:23 +0530

    Add SSL configuration to database connection

--- server.ts
+++ server.ts
@@ -109,6 +109,9 @@ async function initDatabase() {
       waitForConnections: true,
       connectionLimit: 10,
       queueLimit: 0,
+      ,  ssl: {
+rejectUnauthorized: false
+      },
       connectTimeout: 20000 // 20 seconds timeout
     });
 
